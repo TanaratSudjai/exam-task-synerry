9da050d92be81bca550df969d1e34bd4b8b1d377
TANARAT <tanarat.dev18@gmil.com>
2026-05-05 18:01:32 +0700

    fix build db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '',
   database: process.env.MYSQL_DATABASE || 'my_database',
-  port: process.env.MYSQL_PORT || 3306,
+  port: Number(process.env.MYSQL_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
